df6fadc3e06eb6ccbe0b59c293be9a04420d556e
Danielle Gaither <dcgaither@gmail.com>
2018-04-24 18:51:17 -0500

    make sure main name is being retrieved and set correctly

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,21 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+            sandwich.setMainName(jsonSandwich.getJSONObject("name").getString("mainName"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return sandwich;
     }
 }
