32408ddde4ffba3b8acb0062b1c49c7a596d272e
Danielle Gaither <dcgaither@gmail.com>
2018-04-25 17:45:00 -0500

    Updates to UI and JSON parsing

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +44,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +57,10 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        TextView placeOfOriginTextView = (TextView) findViewById(R.id.origin_tv);
+
+        placeOfOriginTextView.setText(sandwich.getPlaceOfOrigin());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,7 +1,10 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,6 +15,11 @@ public class JsonUtils {
         try {
             JSONObject jsonSandwich = new JSONObject(json);
             sandwich.setMainName(jsonSandwich.getJSONObject("name").getString("mainName"));
+            sandwich.setPlaceOfOrigin(jsonSandwich.getString("placeOfOrigin"));
+            sandwich.setDescription(jsonSandwich.getString("description"));
+            JSONArray alsoKnownAs = jsonSandwich.getJSONObject("name").getJSONArray("alsoKnownAs");
+//            String origin = jsonSandwich.getString("placeOfOrigin");
+//            Log.d("DCG", "Origin: " + origin);
         } catch (JSONException e) {
             e.printStackTrace();
             return null;
